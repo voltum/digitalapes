4c4865c7373dc77d78db9fe2468ee499ffeea33c
Voltum8 <mutalov02@mail.ru>
2022-12-16 00:05:23 +0500

    fix: styles are adjusted according the design

--- components/Contacts.js
+++ components/Contacts.js
@@ -3,89 +3,99 @@ import { useRef } from 'react'
 import styles from '../styles/Home.module.css'
 import{ init, send } from 'emailjs-com';
 import cogoToast from 'cogo-toast';
+import Image from "next/image";
+// Icons
+import mail from "../public/img/mail.svg";
+import location from "../public/img/location.svg";
 
 init("user_6oHWAQwVEQeJyG3zXSwng");
 
 function Contacts() {
-    const submitBtn = useRef();
+  const submitBtn = useRef();
 
-    const onSubmit = (e) => {
-      e.preventDefault();
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-      const formData = new FormData(e.target);
+    const formData = new FormData(e.target);
 
-      const params = {
-        from_domain: window.location.hostname,
-        from_name: "DitialApes robot",
-        phone_number: formData.get("phone_number"),
-        full_name: formData.get("full_name"),
-        additional_info: formData.get("additional_info"),
-      };
-
-      const btnText = submitBtn.current.innerText;
-
-      submitBtn.current.innerText = "Sending...";
-
-      send("service_s2n9oik", "template_5br0dhe", params)
-        .then(
-          function (response) {
-            submitBtn.current.innerText = "Application sent!";
-            cogoToast.success("Your application has been sent!");
-            setTimeout(() => {
-              submitBtn.current.innerText = btnText;
-            }, 3000);
-          },
-          function (error) {
-            submitBtn.current.innerText = "Error occured!";
-            cogoToast.success("Error while sending your application!");
-            setTimeout(() => {
-              submitBtn.current.innerText = btnText;
-            }, 3000);
-            console.log("FAILED...", error);
-          }
-        )
-        .finally(() => {
-          // submitBtn.current.innerText = "Sending...";
-        });
+    const params = {
+      from_domain: window.location.hostname,
+      from_name: "DitialApes robot",
+      phone_number: formData.get("phone_number"),
+      full_name: formData.get("full_name"),
+      additional_info: formData.get("additional_info"),
     };
 
-    return (
-      <div className={styles.contacts} id={"contacts"}>
-        <div className={`${styles.container} ${styles.container_contacts}`}>
-          <h2>Contacts</h2>
-          <ul className={styles.contacts_line}>
-            <li>
-              <span> </span>ceo@digitalapes.agency
-            </li>
-            <li>
-              <span> </span>Vienna, Austria
-            </li>
-          </ul>
-          <form className={styles.contacts_form} onSubmit={onSubmit}>
-            <input
-              type="tel"
-              name="phone_number"
-              placeholder="Phone number"
-              required
-            />
-            <input
-              type="text"
-              name="full_name"
-              placeholder="Full name"
-              required
-            />
-            <textarea
-              name="additional_info"
-              placeholder="Additional information"
-              required
-            />
-            <button type="submit" ref={submitBtn}>
-              Submit your application
-            </button>
-          </form>
-        </div>
+    const btnText = submitBtn.current.innerText;
+
+    submitBtn.current.innerText = "Sending...";
+
+    send("service_s2n9oik", "template_5br0dhe", params)
+      .then(
+        function (response) {
+          submitBtn.current.innerText = "Application sent!";
+          cogoToast.success("Your application has been sent!");
+          setTimeout(() => {
+            submitBtn.current.innerText = btnText;
+          }, 3000);
+        },
+        function (error) {
+          submitBtn.current.innerText = "Error occured!";
+          cogoToast.success("Error while sending your application!");
+          setTimeout(() => {
+            submitBtn.current.innerText = btnText;
+          }, 3000);
+          console.log("FAILED...", error);
+        }
+      )
+      .finally(() => {
+        // submitBtn.current.innerText = "Sending...";
+      });
+  };
+
+  return (
+    <div className={styles.contacts} id={"contacts"}>
+      <div className={`${styles.container} ${styles.container_contacts}`}>
+        <h2>Contacts</h2>
+        <ul className={styles.contacts_line}>
+          <li>
+            <span>
+              <Image src={mail} />
+            </span>
+            ceo@digitalapes.agency
+          </li>
+          <li>
+            <span>
+              <Image src={location} />
+            </span>
+            Vienna, Austria
+          </li>
+        </ul>
+        <form className={styles.contacts_form} onSubmit={onSubmit}>
+          <input
+            type="tel"
+            name="phone_number"
+            placeholder="Phone number"
+            required
+          />
+          <input
+            type="text"
+            name="full_name"
+            placeholder="Full name"
+            required
+          />
+          <textarea
+            name="additional_info"
+            placeholder="Additional information"
+            required
+          />
+          <button type="submit" ref={submitBtn}>
+            Submit your application
+          </button>
+        </form>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Contacts
--- components/Header.js
+++ components/Header.js
@@ -4,102 +4,105 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 // Resources
-import logo from '../public/img/logo.svg'
-import head_img from '../public/img/head_img.svg'
+import logo from "../public/img/logo.png";
+import head_img from "../public/img/head_img.svg";
 
 function Header() {
-    const mobileNav = useRef();
+  const mobileNav = useRef();
 
-    const navigation = [
-        {
-            href: "#about",
-            anchor: "About us"
-        },
-        {
-            href: "#service",
-            anchor: "Service"
-        },
-        {
-            href: "#contacts",
-            anchor: "Contacts"
-        }
-    ]
+  const navigation = [
+    {
+      href: "#about",
+      anchor: "About us",
+    },
+    {
+      href: "#service",
+      anchor: "Service",
+    },
+    {
+      href: "#contacts",
+      anchor: "Contacts",
+    },
+  ];
 
-    const openNav = () => {
-        document.body.style.overflow = 'hidden';
-        mobileNav.current.style.display = 'block';
-        mobileNav.current.classList.add(styles.head_nav_mobile_animating);
-        setTimeout(() => {
-            mobileNav.current.classList.remove(styles.head_nav_mobile_animating);            
-        }, 1000);
-    }
+  const openNav = () => {
+    document.body.style.overflow = "hidden";
+    mobileNav.current.style.display = "block";
+    mobileNav.current.classList.add(styles.head_nav_mobile_animating);
+    setTimeout(() => {
+      mobileNav.current.classList.remove(styles.head_nav_mobile_animating);
+    }, 1000);
+  };
 
-    const closeNav = () => {
-        document.body.style.overflow = 'auto';
-        mobileNav.current.classList.add(styles.head_nav_mobile_animating_out);
-        setTimeout(() => {
-            mobileNav.current.classList.remove(styles.head_nav_mobile_animating_out);            
-            mobileNav.current.style.display = 'none';
-        }, 1000);
-    }
+  const closeNav = () => {
+    document.body.style.overflow = "auto";
+    mobileNav.current.classList.add(styles.head_nav_mobile_animating_out);
+    setTimeout(() => {
+      mobileNav.current.classList.remove(styles.head_nav_mobile_animating_out);
+      mobileNav.current.style.display = "none";
+    }, 1000);
+  };
 
-    return (
-      <div className={styles.header}>
-        <div className={`${styles.head_nav_wrapper}`}>
-          <nav className={`${styles.container} ${styles.head_nav}`}>
-            <div>
-              <Image src={logo} />
-            </div>
-            <div className={styles.header_nav_block}>
-              <ul>
-                {navigation.map((nav) => (
-                  <li key={nav.href}>
-                    <a href={nav.href}>{nav.anchor}</a>
-                  </li>
-                ))}
-              </ul>
-              <button className={styles.burger_btn} onClick={openNav} />
-            </div>
-          </nav>
-        </div>
-        <nav
-          className={`${styles.head_nav_mobile} ${styles.container}`}
-          ref={mobileNav}
-        >
-          <div className={styles.overlay}></div>
-          <div className={styles.header_nav_mobile_block}>
-            <div className={styles.nav_mobile_header}>
-              <Image src={logo} />
-              <button className={styles.close_btn} onClick={closeNav} />
-            </div>
+  return (
+    <div className={styles.header}>
+      <div className={`${styles.head_nav_wrapper}`}>
+        <nav className={`${styles.container} ${styles.head_nav}`}>
+          <div>
+            <Image src={logo} />
+          </div>
+          <div className={styles.header_nav_block}>
             <ul>
               {navigation.map((nav) => (
-                <li key={nav.href} onClick={closeNav}>
+                <li key={nav.href}>
                   <a href={nav.href}>{nav.anchor}</a>
                 </li>
               ))}
             </ul>
-            <div className={styles.nav_mobile_footer}>
-              <span>ceo@digitalapes.agency</span>
-            </div>
+            <button className={styles.burger_btn} onClick={openNav} />
           </div>
         </nav>
-        <div className={styles.container}>
-          <div className={styles.head_block_wrapper}>
-            <div className={styles.head_block}>
-              <h1>Our objective</h1>
-              <p>
-                Full complex of services in promotion of Internet turnkey
-                projects worldwide, from Europe to Asia. Deep dive into a
-                project, ensure effective work with the target audience and
-                business development of clients in the network. Thanks to it
-                increase brand recognition and conversion
-              </p>
-            </div>
+      </div>
+      <nav
+        className={`${styles.head_nav_mobile} ${styles.container}`}
+        ref={mobileNav}
+      >
+        <div className={styles.overlay}></div>
+        <div className={styles.header_nav_mobile_block}>
+          <div className={styles.nav_mobile_header}>
+            <Image src={logo} />
+            <button className={styles.close_btn} onClick={closeNav} />
+          </div>
+          <ul>
+            {navigation.map((nav) => (
+              <li key={nav.href} onClick={closeNav}>
+                <a href={nav.href}>{nav.anchor}</a>
+              </li>
+            ))}
+          </ul>
+          <div className={styles.nav_mobile_footer}>
+            <span>ceo@digitalapes.agency</span>
+          </div>
+        </div>
+      </nav>
+      <div className={styles.container}>
+        <div className={styles.head_block_wrapper}>
+          <div className={styles.head_block}>
+            <h1>Our objective</h1>
+            <p>
+              Full complex of services in promotion of Internet turnkey projects
+              worldwide, from Europe to Asia. Deep dive into a project, ensure
+              effective work with the target audience and business development
+              of clients in the network. Thanks to it increase brand recognition
+              and conversion
+            </p>
+            <a href="#contacts" className={styles.btn}>
+              Start
+            </a>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Header
